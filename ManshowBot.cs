d5d38bc78c136205f298b697cca4b47e23830fb3
Angusm3 <treeforge.al@gmail.com>
2016-07-16 14:07:08 -0700

    dice parsing almost done

--- ManshowBot.cs
+++ ManshowBot.cs
@@ -36,37 +36,47 @@ namespace DiscordBot_01
 
 
 
-            if (e.Message.Text.Contains ("roll"))
+            if (e.Message.Text.Contains ("roll "))
             {
+                
+                string text = e.Message.Text;
+
+                text = text.Replace("roll ", "");
+
+
                 char[] delimiterChars = { 'd', ' ' };
 
-                string text = e.Message.Text;
-                e.Channel.SendMessage("Original text: " + text);
+
+//                e.Channel.SendMessage("Original text: " + text);
 
                 string[] words = text.Split(delimiterChars);
-                e.Channel.SendMessage("numbers in roll:" + words.Length);
+//                e.Channel.SendMessage("numbers in roll:" + words.Length);
+
 
 
 
                 int[] DieArray = text.Split('d').Select(str => int.Parse(str)).ToArray();
                 Console.WriteLine(text);
-
-
-
+                if (DieArray[1] > 1000)
+                    DieArray[1] = 1000;
 
                 string[] DieString = DieArray.Select(x => x.ToString()).ToArray();
 
-                for (int i = 0; i < DieArray[0]; ++i)
+                e.Channel.SendMessage("Dice Rolled: " + DieString[0] + "d" + DieString[1]);
 
+
+
+
+                for (int i = 0; i < DieArray[0]; ++i)
                 {
                     Random rnd = new Random();
                     DiceRoll var = new DiscordBot_01.DiceRoll();
                     e.Channel.SendMessage("Rolled .. " + rnd.Next(1, DieArray[1] + 1));
-                    e.Channel.SendMessage("Dice Rolled: " + DieString[0]);
+
                     
                 }
 
-
+//              e.Channel.SendMessage(DieString[1]);
 
 
 
